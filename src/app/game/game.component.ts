2a71b15ccf6aa3ef31fb0bc87e8d5f5582bc8844
Axel Pröger <a.proeger@gmail.com>
2024-04-09 20:42:38 +0200

    added card stack

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrl: './game.component.scss',
 })
-export class GameComponent {
-
-}
+export class GameComponent {}
